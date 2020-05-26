fad1972ae1398d88d2f7474cc4f44de4715ebca3
Connor Moody <cwmlolzlz@gmail.com>
2020-05-26 20:17:27 +1200

    Version bump to 0.0.8

--- src/SkillsPlugin.cs
+++ src/SkillsPlugin.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace SkillsPlusPlus {
     
     [BepInDependency ("com.bepis.r2api")]
-    [BepInPlugin ("com.cwmlolzlz.skills", "Skills", "0.0.7")]
+    [BepInPlugin ("com.cwmlolzlz.skills", "Skills", "0.0.8")]
     [R2APISubmoduleDependency("AssetPlus")]
     public class PhotoModePlugin : BaseUnityPlugin {
 
