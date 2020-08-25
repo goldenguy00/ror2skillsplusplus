73e16870b88321373bc20f1bad13261b185dcb83
Connor Moody <cwmlolzlz@gmail.com>
2020-08-25 23:04:30 +1200

    Added modifiers to lunar item abilities

---
+++ Source/Modifiers/LunarModifiers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using EntityStates.GlobalSkills.LunarNeedle;
+using EntityStates;
+
+using RoR2;
+using RoR2.Skills;
+
+namespace SkillsPlusPlus.Modifiers {
+    class LunarModifiers {
+
+        [SkillLevelModifier("LunarPrimaryReplacement", typeof(FireLunarNeedle))]
+        class VisionsOfHeresySkillModifier : SimpleSkillModifier<FireLunarNeedle> {
+
+            public override void OnSkillLeveledUp(int level, CharacterBody characterBody, SkillDef skillDef) {
+                base.OnSkillLeveledUp(level, characterBody, skillDef);
+                skillDef.baseMaxStock = AdditiveScaling(12, 2, level);
+                FireLunarNeedle.damageCoefficient = MultScaling(1.2f, 0.2f, level);
+            }
+
+        }
+
+        //class StridesOfHeresySkillModifier : SimpleSkillModifier<GhostUtilitySkillState> {
+
+        //    public override void OnSkillLeveledUp(int level, CharacterBody characterBody, SkillDef skillDef) {
+        //        base.OnSkillLeveledUp(level, characterBody, skillDef);
+        //        skillDef.baseMaxStock = AdditiveScaling(12, 2, level);
+        //        FireLunarNeedle.damageCoefficient = MultScaling(1.2f, 0.2f, level);
+        //    }
+
+        //}
+
+    }
+}
