831af661557a40efdda619e4a93bed70c110fe5a
Connor Moody <cwmlolzlz@gmail.com>
2020-09-09 21:45:52 +1200

    Added beta log message

--- Source/SkillsPlugin.cs
+++ Source/SkillsPlugin.cs
@@ -1,10 +1,9 @@
 using BepInEx;
-using UnityEngine;
-
 using R2API;
 using R2API.Utils;
-using SkillsPlusPlus.Modifiers;
 using RoR2.UI;
+using SkillsPlusPlus.Modifiers;
+using UnityEngine;
 using UnityEngine.Networking;
 
 namespace SkillsPlusPlus {
@@ -12,17 +11,32 @@ namespace SkillsPlusPlus {
     [BepInDependency ("com.bepis.r2api")]
     [BepInPlugin ("com.cwmlolzlz.skills", "Skills", "0.1.5")]
     [R2APISubmoduleDependency(nameof(CommandHelper), nameof(LanguageAPI))]
-    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod)]
     public sealed class SkillsPlugin : BaseUnityPlugin {
 
         private SkillPointsController localSkillPointsController;
+        private GameObject modLabel;
 
         //private SkillPointsController skillsController;
         //private SkillLevelIconController[] skillsHUDControllers;
 
         void Awake() {
 
-#if DEBUG
+            SkillsPlusPlus.Logger.Warn(@"
+  _____  _     _  _  _                       ____         _         
+ / ____|| |   (_)| || |        _      _     |  _ \       | |        
+| (___  | | __ _ | || | ___  _| |_  _| |_   | |_) |  ___ | |_  __ _ 
+ \___ \ | |/ /| || || |/ __||_   _||_   _|  |  _ <  / _ \| __|/ _` |
+ ____) ||   < | || || |\__ \  |_|    |_|    | |_) ||  __/| |_| (_| |
+|_____/ |_|\_\|_||_||_||___/                |____/  \___| \__|\__,_|
+
+Note: You are running the Skills++ {0} beta.
+This is a pre-release and is to guarenteed to be stable, bug free, or crash free.
+
+Raise bugs here https://discord.gg/wU94CjJ
+            ", this.Info.Metadata.Version.ToString());
+
+            #if DEBUG
             SkillsPlusPlus.Logger.LOG_LEVEL = SkillsPlusPlus.Logger.LogLevel.Debug;
             UnityEngine.Networking.LogFilter.currentLogLevel = LogFilter.Info;
 
@@ -41,13 +55,13 @@ namespace SkillsPlusPlus {
 
             bool didAttemptToConnect = false;
             On.RoR2.UI.MainMenu.MainMenuController.Start += (orig, self) => {
-                if(didAttemptToConnect == false) {
+                if (didAttemptToConnect == false) {
                     didAttemptToConnect = true;
                     RoR2.Console.instance.SubmitCmd(null, "connect 192.168.1.102:27015", true);
                 }
                 orig(self);
             };
-#endif
+            #endif
 
             CommandHelper.AddToConsoleWhenReady();
 
@@ -62,7 +76,6 @@ namespace SkillsPlusPlus {
             playerMasterPrefab.EnsureComponent<SkillPointsController>();
 
             On.RoR2.UI.HUD.Awake += this.HUD_Awake;
-
         }
 
         private void HUD_Awake(On.RoR2.UI.HUD.orig_Awake orig, RoR2.UI.HUD self) {
