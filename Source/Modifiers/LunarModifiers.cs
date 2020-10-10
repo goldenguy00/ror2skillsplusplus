268bc16206c8c483e862ef6cd1e68531ee55f5f2
Connor Moody <cwmlolzlz@gmail.com>
2020-10-10 16:03:01 +1300

    Added skill modifier for Visions of Heresey item

--- Source/Modifiers/LunarModifiers.cs
+++ Source/Modifiers/LunarModifiers.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
-using EntityStates.GlobalSkills.LunarNeedle;
 using EntityStates;
-
+using EntityStates.GlobalSkills.LunarNeedle;
 using RoR2;
 using RoR2.Skills;
 
@@ -22,15 +20,16 @@ namespace SkillsPlusPlus.Modifiers {
 
         }
 
-        //class StridesOfHeresySkillModifier : SimpleSkillModifier<GhostUtilitySkillState> {
+        [SkillLevelModifier("LunarUtilityReplacement", typeof(GhostUtilitySkillState))]
+        class StridesOfHeresySkillModifier : SimpleSkillModifier<GhostUtilitySkillState> {
 
-        //    public override void OnSkillLeveledUp(int level, CharacterBody characterBody, SkillDef skillDef) {
-        //        base.OnSkillLeveledUp(level, characterBody, skillDef);
-        //        skillDef.baseMaxStock = AdditiveScaling(12, 2, level);
-        //        FireLunarNeedle.damageCoefficient = MultScaling(1.2f, 0.2f, level);
-        //    }
+            public override void OnSkillLeveledUp(int level, CharacterBody characterBody, SkillDef skillDef) {
+                base.OnSkillLeveledUp(level, characterBody, skillDef);
+                GhostUtilitySkillState.moveSpeedCoefficient = MultScaling(1.3f, 0.1f, level); // +10% 
+                GhostUtilitySkillState.healFrequency = MultScaling(5, 0.15f, level);
+            }
 
-        //}
+        }
 
     }
 }
